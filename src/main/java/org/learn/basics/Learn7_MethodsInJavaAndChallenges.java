4da131c2d63973f2de7271b2946a7da39702d8c5
amankhanp <pamanullahkhan.aman@gmail.com>
2020-02-05 18:07:50 +0530

    added class explaining method and solved one challenge related to it.

--- src/main/java/org/learn/basics/Learn7_MethodsInJavaAndChallenges.java
+++ src/main/java/org/learn/basics/Learn7_MethodsInJavaAndChallenges.java
@@ -1,6 +1,25 @@
 package org.learn.basics;
 
-public class Learn_Temp {
+/**
+ * ### Challenge-4 ###
+ *
+ * Create a method called displayHighScore
+ * It should accept a player name as a parameter, and 2nd parameter as a position in the high score table
+ * You should display the players name along with a message including playerName and his position
+ * and the position they got.
+ *
+ * Create a 2nd method called calculateHighScorePosition
+ * it should be sent one argument only, the player score.
+ * it should return an int
+ * the return data should be
+ * 1 if the score is >1000
+ * 2 if the score is >500 and <1000
+ * 3 if the score is >100 and <500
+ * 4 in all other cases
+ * call both methods and display the results of the following
+ * a score of 1500, 900, 400 and 50.
+ */
+public class Learn7_MethodsInJavaAndChallenges {
 
     public static void main(String[] args) {
         int highScore = calculateScore(true, 800, 5, 100);
@@ -42,10 +61,22 @@ public class Learn_Temp {
             return 2;
         } else if (playerScore >= 100 && playerScore < 500) { //Here playerScore < 500 will always return true, when reached. we can simply it but removing it.
             return 3;
+        } else {
+            return 4;
         }
-        return 4;
-         */
 
+        Optimization process -2
+        if (playerScore >= 1000) {
+            return 1;
+        } else if (playerScore >= 500) {
+            return 2;
+        } else if (playerScore >= 100) {
+            return 3;
+        } else {
+            return 4;
+        }
+
+        Optimization process -3
         if (playerScore >= 1000) {
             return 1;
         } else if (playerScore >= 500) {
@@ -54,5 +85,20 @@ public class Learn_Temp {
             return 3;
         }
         return 4;
+         */
+
+        //Optimization process -4
+        /**
+         * In this syntax, created created Local variable which returns a position value.
+         */
+        int position = 4;
+        if (playerScore >= 1000) {
+            position = 1;
+        } else if (playerScore >= 500) {
+            position = 2;
+        } else if (playerScore >= 100) {
+            position = 3;
+        }
+        return position;
     }
 }
