50dcf0cf3ee36a1f68db99a3bba2f3184af85b71
Aman Khan <iamankhanp@gmail.com>
2024-03-19 15:06:41 -0400

    Updated PrimitiveTypes java class

--- src/main/java/org/learn/basics/Learn1_IntByteShortLongPrimitiveTypes.java
+++ src/main/java/org/learn/basics/Learn1_IntByteShortLongPrimitiveTypes.java
@@ -4,12 +4,12 @@ package org.learn.basics;
  * In this class we are going to understand better about type value overflow
  * and underflow.
  *
- * also few examples related to 'int,byte,short and long' they are also know
+ * also few examples related to 'int,byte,short and long' they are also known
  * as 'Whole numbers'.
  * note: Java treats 'int' as a default whole number.
  *
- * Simply we are create int type variables and check its max and min value.
- * then to try to add max + 1 and min -1.
+ * Simply we are creating int type variables and checking its max and min value.
+ * then to trying to add max + 1 and min -1.
  *
  * Explanation: if we try to put a value larger than the maximum value in java,
  * or a value smaller than the minimum value in java, then we will get an
@@ -31,8 +31,8 @@ public class Learn1_IntByteShortLongPrimitiveTypes {
         System.out.println("myMinIntValue2 : " + myMinIntValue2); // output: -2147483648
 
         /*
-        If you have given 'int' value more than its max or less then its min you will
-        get an compile time error saying Integer value is too large.
+        If you have given 'int' value more than its max or less than its min, then you will
+        get a compile time error saying Integer value is too large.
         for checking uncomment the below 2 lines and compile the code.
          */
         //int myMaxIntValue3 = 2147483648; //Integer value is too large.
@@ -51,9 +51,9 @@ public class Learn1_IntByteShortLongPrimitiveTypes {
         /**
          * Another great example to understand Overflow
          * Here 'value'(variable type int) is assigned with
-         * 2147483647 (2147483648 - 1), after the for loop runs
+         * 2147483647 (2147483647 - 1), after the for loop runs
          * it will print the value and then increment its value by 1
-         * output will be 2147483648 again increment by 1 based on logic
+         * output will be 2147483647 again increment by 1 based on logic
          * then overflow take place and output will be -2147483648 again
          * after increment output is -2147483647.
          */
@@ -79,10 +79,10 @@ public class Learn1_IntByteShortLongPrimitiveTypes {
 
         long myLongValue1 = 2147483647;
         /**
-         * if you declare long value without suffix 'L', it consider under Integer value.
+         * if you declare long value without suffix 'L', it considers under Integer value.
          * to check uncomment the below line and compile the code.
          */
-        //long myLongValue2 = 2147483648; //Integer number too large
+        //long myLongValue2 = 2147483648; // compile time error: Integer number too large
         long myLongValue3 = 2147483648674576L;
         System.out.println("myLongValue3 is : "+ myLongValue3);
     }
