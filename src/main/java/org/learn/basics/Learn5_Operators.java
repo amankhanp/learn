76dab2edf091b23598140509dcd39a4d86d4adeb
Aman Khan <iamankhanp@gmail.com>
2024-07-02 14:27:48 -0400

    Updated Operators java class

--- src/main/java/org/learn/basics/Learn5_Operators.java
+++ src/main/java/org/learn/basics/Learn5_Operators.java
@@ -1,22 +1,23 @@
 package org.learn.basics;
 
 /**
- * In this class we discuss about operators.
+ * In this class, we discuss operators.
  *
- * Read about Summary of Operators: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/opsummary.html
+ * Read about the Summary of Operators: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/opsummary.html
  * Read about Java Operator Precedence Table: http://www.cs.bilkent.edu.tr/~guvenir/courses/CS101/op_precedence.html
  */
 public class Learn5_Operators {
 
     public static void main(String[] args) {
         /**
-         * Here, We see =, ==. We use '=' sign to assign a value to a variable.
-         * which is also know as assignment operator and '==' is known as
-         * equals-to operator through which we compare two values.
-         * In the below given example 'if' condition accepts boolean value, == is
-         * an 'operator' myIntValue and 50 are 'operands' and myIntValue == 50
-         * is an 'expression'. myIntValue == 50, it means myIntValue is equal to 50
-         * or not, true or false.
+         * Here, We see "=", "==".
+         * We use '=' sign to assign a value to a variable.
+         * Which is also known as assignment operator and '==' is known as equals-to operator
+         * through which we compare two values.
+         *
+         * In the below-given example 'if' condition accepts boolean value, == is an 'operator'
+         * myIntValue and 50 are 'operands' and myIntValue == 50 is an 'expression'.
+         * myIntValue == 50, it means myIntValue is equal to 50 or not, true or false.
          */
         int myIntValue = 50;
         if (myIntValue == 50) {
@@ -26,7 +27,7 @@ public class Learn5_Operators {
         /**
          * In the below example, We have a boolean variable myBooleanValue is false,
          * and in if condition we are changing its value with true, overall if(true)
-         * which is valid condition. This is not a recommended way, example is just
+         * which is a valid condition. This is not a recommended way; example is just
          * to make clear we can use assignment operators to boolean variables in if
          * condition.
          */
@@ -39,7 +40,7 @@ public class Learn5_Operators {
          * Ternary Operator: it is a shortcut to assigning one or two values to a
          * variable depending on a given condition.
          *
-         * it's a shortcut of the if-then-else statement.
+         * It's a shortcut of the if-then-else statement.
          */
         int ageOfClient = 20;
         boolean wasAgeOfClient = ageOfClient == 20;
@@ -66,11 +67,11 @@ public class Learn5_Operators {
         if(!isMyChallengeReminderValueTrue){
             System.out.println("Got Some Reminder");
             /**
-             * value is not 0.00 because of the below syntax.
-             * syntax1: myChallengeDoubleValue1 + myChallengeDoubleValue2 * 100.00d;
-             * '*' has high precedence than '+' so first it multiplies and then addition
-             * took place. To get correct output use below syntax.
-             * syntax2: (myChallengeDoubleValue1 + myChallengeDoubleValue2) * 100.00d;
+             * Value is not 0.00 because of the below syntax.
+             * Syntax1: myChallengeDoubleValue1 + myChallengeDoubleValue2 * 100.00d;
+             * '*' has high precedence than '+' so first it multiplies and then addition took place.
+             * To get the correct output use below syntax.
+             * Syntax2: (myChallengeDoubleValue1 + myChallengeDoubleValue2) * 100.00d;
              */
         }
     }
