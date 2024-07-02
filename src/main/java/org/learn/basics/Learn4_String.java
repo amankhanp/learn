e057445deb2b361f69750faad50eba9180011553
Aman Khan <iamankhanp@gmail.com>
2024-07-02 14:18:16 -0400

    Updated String java class

--- src/main/java/org/learn/basics/Learn4_String.java
+++ src/main/java/org/learn/basics/Learn4_String.java
@@ -2,17 +2,18 @@ package org.learn.basics;
 
 /**
  * String: The String is a data-type in a java, which is not a primitive-type.
- * it's actually a Class, but it enjoys a bit of favoritism in java to make
+ * It's actually a Class, but it enjoys a bit of favoritism in java to make
  * it easier to use than a regular class.
  *
- * A String is a sequence of a characters. A large no.of characters. Technically
- * it's limited by memory.
+ * A String is a sequence of characters.
+ * A large number of characters.
+ * Technically, it's limited by memory.
  *
- * ### String in java are immutable ###
- * When i said you can delete characters out of String, that's not strictly true.
- * Because Strings in java are immutable. That means you can't change a String
- * after it's created. Instead, what happens is a new String is created.
- * read: https://mkyong.com/java/java-mutable-and-immutable-objects/
+ * ### String in java is immutable ###
+ * When I said you can delete characters out of String, that's not strictly true.
+ * Because Strings in java are immutable. That means you can't change a String after it's created.
+ * Instead, what happens is a new String is created.
+ * Read: https://mkyong.com/java/java-mutable-and-immutable-objects/
  */
 public class Learn4_String {
 
@@ -20,7 +21,7 @@ public class Learn4_String {
         String myStringValue = "This is a String";
         System.out.println("myStringValue is: " + myStringValue);
 
-        myStringValue = myStringValue + "and this is more.";
+        myStringValue = myStringValue + " and this is more.";
         System.out.println("myStringValue is: " + myStringValue);
 
         myStringValue = myStringValue + "\u00A9 2020";
@@ -35,16 +36,16 @@ public class Learn4_String {
          * Adding both the values and storing it in string variable then what could be
          * the output.
          *
-         * simple, Java is smart enough to convert int value as string will whole statement
+         * Simple, Java is smart enough to convert int value as string will whole statement
          * is assigned to string.
          *
-         * note: as we read in class description, Java Strings are immutable.
+         * Note: as we read in class description, Java Strings are immutable.
          * In this case of this code, lastStringValue doesn't get appended the value "50"
          * instead a new String is created which consists of the previous value lastStringValue
          * plus a text representation of the int value myIntValue 10.
          *
-         * In net result is the same, lastStringValue has the right values, however, a new
-         * String got created and old one got discarded from memory automatically by java.
+         * In net result it is the same, lastStringValue has the right values, however, a new
+         * String got created, and old one got discarded from memory automatically by java.
          */
         String lastStringValue = "50";
         int myIntValue = 10;
