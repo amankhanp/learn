53b1e014f5dd3210f3299ac1021e2784a9f60276
Aman Khan <iamankhanp@gmail.com>
2024-07-02 14:42:24 -0400

    Updated Methods java class

--- src/main/java/org/learn/basics/Learn7_MethodsInJavaAndChallenges.java
+++ src/main/java/org/learn/basics/Learn7_MethodsInJavaAndChallenges.java
@@ -3,19 +3,19 @@ package org.learn.basics;
 /**
  * ### Challenge-4 ###
  *
- * Create a method called displayHighScore
- * It should accept a player name as a parameter, and 2nd parameter as a position in the high score table
- * You should display the players name along with a message including playerName and his position
+ * Create a method called displayHighScore. It should accept a player name as a parameter,
+ * and second parameter as a position in the high-score table.
+ * You should display the player's name along with a message including playerName and his position
  * and the position they got.
  *
- * Create a 2nd method called calculateHighScorePosition
+ * Create a second method called calculateHighScorePosition
  * it should be sent one argument only, the player score.
  * it should return an int
  * the return data should be
  * 1 if the score is >1000
  * 2 if the score is >500 and <1000
  * 3 if the score is >100 and <500
- * 4 in all other cases
+ * 4 in all other cases,
  * call both methods and display the results of the following
  * a score of 1500, 900, 400 and 50.
  */
@@ -27,7 +27,7 @@ public class Learn7_MethodsInJavaAndChallenges {
         int negativeScore = calculateScore(false, 10000, 8, 200);
         System.out.println(negativeScore);
 
-        int highScorePosition = calculateHighScorePosition(1000);
+        int highScorePosition = calculateHighScorePosition(1500);
         displayHighScore("Aman", highScorePosition);
         highScorePosition = calculateHighScorePosition(900);
         displayHighScore("Khan", highScorePosition);
@@ -35,6 +35,8 @@ public class Learn7_MethodsInJavaAndChallenges {
         displayHighScore("Pathan", highScorePosition);
         highScorePosition = calculateHighScorePosition(50);
         displayHighScore("Amanullah", highScorePosition);
+        highScorePosition = calculateHighScorePosition(1000);
+        displayHighScore("AKP", highScorePosition);
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
@@ -57,10 +59,10 @@ public class Learn7_MethodsInJavaAndChallenges {
         Optimization process -1
         if (playerScore >= 1000) {
             return 1;
-        } else if (playerScore >= 500 && playerScore < 1000) { //Here playerScore < 1000 will always return true, when reached. we can simply it but removing it.
-            return 2;
-        } else if (playerScore >= 100 && playerScore < 500) { //Here playerScore < 500 will always return true, when reached. we can simply it but removing it.
-            return 3;
+        } else if (playerScore >= 500 && playerScore < 1000) { //Here playerScore < 1000 will always return true, when reached. We can simply remove it.
+            Return 2;
+        } else if (playerScore >= 100 && playerScore < 500) { //Here playerScore < 500 will always return true, when reached. We can simply remove it.
+            Return 3;
         } else {
             return 4;
         }
@@ -89,7 +91,7 @@ public class Learn7_MethodsInJavaAndChallenges {
 
         //Optimization process -4
         /**
-         * In this syntax, created created Local variable which returns a position value.
+         * In this syntax, created Local variable, which returns a position value.
          */
         int position = 4;
         if (playerScore >= 1000) {
