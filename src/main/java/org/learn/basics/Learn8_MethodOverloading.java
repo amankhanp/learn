e5f4c0db73574ce0ecc670ac10651bc1e253eeb5
Aman Khan <iamankhanp@gmail.com>
2024-07-02 14:52:26 -0400

    Updated Method Overloading java class

--- src/main/java/org/learn/basics/Learn8_MethodOverloading.java
+++ src/main/java/org/learn/basics/Learn8_MethodOverloading.java
@@ -1,18 +1,17 @@
 package org.learn.basics;
 
 /**
- * Concept: Method Overloading
- * Method Overloading is a feature that allows us to have more than one method with the same name,
+ * Concept: Method Overloading is a feature that allows us to have more than one method with the same name,
  * so as long as we use different parameters.
  *
- * It is ability to create multiple methods of the same name with different implementations.
+ * It is an ability to create multiple methods of the same name with different implementations.
  *
  * Calls to an overloaded method will run a specific implementation of that method.
  *
  * Advantages:
  * > It Improves the code readability and re-usability.
  * > It is easier to remember one method name instead of remembering multiple names.
- * > Achieves consistency in naming. One name for methods that are commonly used for example println.
+ * > Achieves consistency in naming. One name for methods that are commonly used, for example, println.
  * > Overloaded methods give programmers the flexibility to call a similar method with different types
  * of data.
  */
@@ -47,24 +46,24 @@ public class Learn8_MethodOverloading {
     /**
      * ### Challenge-7 ###
      * Create a method called getDurationString with two params, 1st is minutes and 2nd is seconds.
-     * You should validate that the 1st param minutes is >=0.
-     * also validate that the 2nd param seconds is >=0 & <=59.
-     * then return "Invalid Value" in the method if either of the above are not true.
+     * You should validate that the first param minutes is >=0.
+     * Also validate that the second param seconds is >=0 & <=59.
+     * Then return "Invalid Value" in the method if either of the above is not true.
      *
-     * If the parameters are valid then calculate how many hours minutes and seconds equal the minutes
+     * If the parameters are valid, then calculate how many hours minutes and seconds equal the minutes
      * and seconds passed to this method and return that value as string in format "XXh YYm ZZs" where
      * XX represents a number of hours, YY the minutes and ZZ the seconds.
      *
-     * Create a 2nd method of the same name but with only one parameter seconds.
+     * Create a second method of the same name but with only one parameter seconds.
      * Validate that it is >=0, and return "Invalid value" if it is not true.
-     * If it is valid, then calculate how many minutes are in the seconds value and then call
+     * If it is valid, then calculate how many 'minutes' are in the 'seconds' value and then call
      * the overloaded method passing the correct minutes and seconds calculated so that it can
      * calculate correctly.
      *
-     * call both methods to print values to the console.
+     * Call both methods to print values to the console.
      *
      * Tips:
-     * >Use int or long for your number data types is probably a good idea.
+     * >Use int or long for your number-data-types is probably a good idea.
      * >1 minute = 60seconds and 1 hour = 60minutes or 3600seconds.
      *
      * Bonus:
